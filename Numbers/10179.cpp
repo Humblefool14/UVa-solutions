1bc698265668acbca83c98c8b84f839b09bfdc7e
Humblefool14 <32316012+Humblefool14@users.noreply.github.com>
2020-05-05 01:00:06 +0530

    Add files via upload

---
+++ Numbers/10179.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+int phi (int n) {
+	int result = n;
+	int i;
+	for ( i=2; i*i<=n; ++i)
+		if (n % i == 0) {
+			while (n % i == 0)
+				n /= i;
+			result -= result / i;
+		}
+	if (n > 1)
+		result -= result / n;
+	return result;
+}
+
+int main()
+{
+    int n;
+    while(cin >> n && n!=0)
+	{
+        cout<< phi(n) << endl;
+    }
+    return 0;
+}
