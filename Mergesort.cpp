8d0bb55862fa7ccdacd99e68be9629a1100dd3a3
Humblefool14 <32316012+Humblefool14@users.noreply.github.com>
2020-07-01 23:04:54 +0530

    Update Mergesort.cpp

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -5,17 +5,13 @@ void merge(int arr[], int l, int m, int r)
     int i, j, k;
     int n1 = m - l + 1;
     int n2 = r - m;
-    /* create temp arrays */
-    int L[n1], R[n2];
-    /* Copy data to temp arrays L[] and R[] */
+    int L[n1], R[n2]; // Temp arrays + Copy data to them.
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
     /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
+    i = 0;j = 0;k = l; 
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             arr[k] = L[i];
@@ -27,15 +23,12 @@ void merge(int arr[], int l, int m, int r)
         }
         k++;
     }
-    /* Copy the remaining elements of L[], if there
-       are any */
+    // copy remaining elements
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
-    /* Copy the remaining elements of R[], if there
-       are any */
     while (j < n2) {
         arr[k] = R[j];
         j++;
